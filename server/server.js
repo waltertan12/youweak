221a5f528cc680df5ce2b5205d04444bda64af61
Walter Tan <wtan@coursehero.com>
2016-08-05 10:08:14 -0700

    Add space between pokefusion image and text

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.post('/weak', (req, res) => {
         case Commands.pokefusion:
             return res.json({
                 response_type: 'in_channel',
-                text: 'Wow, an inspiring new pokemon that\'s probably stronger than you' + 
+                text: 'Wow, an inspiring new pokemon that\'s probably stronger than you ' + 
                     Utils.getPokeFusion(req.body.text)
             });
 
